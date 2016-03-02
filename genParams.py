a17542ea0fe7418147801c36b73e9e4e2a00e88c
bharris12 <bharris@gmail.com>
2016-03-02 12:50:37 -0500

    add parameter generator program

--- genParams.py
+++ genParams.py
@@ -5,10 +5,14 @@ number_of_cables = input("Enter the number of cables you want: ")
 
 #num_params = input("Enter the number of paramaters you want to randomize: ")
 num_params = 17
+
+
+# if you want to make the parameter file include the constants for the model
 #for i in range(0, num_params-1):
 #    param_value[i] = input("enter the #%d parameter that you want to change" % (i + 1,))
-param_value = [14.838, 5.405, .153, .0146, .1238, 2.9e-4, 5.92e-4, .073, .392, 3.98e-5, 2.724, 1000, 3.6e-4, 6.375, .102, 1.27, 526]
-print(len(param_value))
+#param_value = [14.838, 5.405, .153, .0146, .1238, 2.9e-4, 5.92e-4, .073, .392, 3.98e-5, 2.724, 1000, 3.6e-4, 6.375, .102, 1.27, 526]
+#print(len(param_value))
+
 file_name = raw_input("Create a name for the parameter set you want to create: ")
 
 file_name_1D = file_name + "_1D.txt"
@@ -22,8 +26,8 @@ for i in range(1,number_of_cables):
     
     for j in range(1,num_params):
         rand_param_1 = random.gauss(0,.15)
-        file_1D.write(str(param_value[i]*rand_param_1)+ '\t') # writes value for the 1 cell in a cable
-        file_2D.write(str(param_value[i]*rand_param_1)+ '\t') # same first value in 1D and 2D cables
+        file_1D.write(str(rand_param_1)+ '\t') # writes value for the 1 cell in a cable
+        file_2D.write(str(rand_param_1)+ '\t') # same first value in 1D and 2D cables
         for n in range(2,length_of_cables):
             rand_param_n = random.gauss(rand_param_1, .15)  # generates value normally distributed around the first parameter
             file_1D.write(str(rand_param_1 * param_value[i]) + '\t') # repeats first value for 1D
