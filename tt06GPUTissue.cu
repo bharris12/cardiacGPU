c0b93e4c4d08245e6663cf731d0a2a170ea18c67
bharris12 <bharris@gmail.com>
2016-04-17 16:37:11 -0400

    Confirmed that Voltage is propogating stimulus

--- tt06GPUTissue.cu
+++ tt06GPUTissue.cu
@@ -495,106 +495,110 @@ __global__ void compute_voltage(float* x, float* V, float* Iion, float step, flo
 	//}
 
 	// Cable Model
-	if (width == 1) {
-		if (idx == 0) { //first + stimulus
-			if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n + 1] - x[n])) - (Iion[m] + Istim1) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n + 1] - x[n])) - (Iion[m] + Istim1) / Cm))) {
-				V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n + 1] - x[n])) - (Iion[m] + Istim1) / Cm);
-				//V[m] = (x[n]) + (step)*( rad/(2*rho*Cm*deltx*deltx)*(x[n+1]-2*x[n] + x[n+length-1]) - (Iion[n]+Istim1) /Cm ) ; // loop
-			}
-			else { V[m] = x[n]; }
-		}
-		else if (idx == num_cells - 1){ //last
-			if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(-x[n] + x[n - 1]) - (Iion[m]) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(-x[n] + x[n - 1]) - (Iion[m]) / Cm))) {
-				V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(-x[n] + x[n - 1]) - (Iion[m]) / Cm);
-				//V[m] = (x[n]) + (step)*( rad/(2*rho*Cm*deltx*deltx)*(x[n+1-length] - 2*x[n] + x[n-1]) - (Iion[n]) /Cm ); // loop
-			}
-			else { V[m] = x[n]; }
-		}
-		else if (idx == 1) { //need stimulus
-			if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm))) {
-				V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm);
-			}
-			else { V[m] = x[n]; }
-		}
-		else if (idx == 2) { //need stimulus
-			if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm))) {
-				V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm);
-			}
-			else { V[m] = x[n]; }
-		}
-		//stim2
-		else if (s2_analysis && s2_loc == idx) { //need stimulus
-			if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm))) {
-				V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm);
-			}
-			else { V[m] = x[n]; }
-		}
-		else if (s2_analysis && s2_loc + 1 == idx) { //need stimulus
-			if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm))) {
-				V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm);
-			}
-			else { V[m] = x[n]; }
-		}
-		else if (s2_analysis && s2_loc - 1 == idx) { //need stimulus
-			if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm))) {
-				V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm);
-			}
-			else { V[m] = x[n]; }
-		}
-		else {
-			if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n - 1] - x[n]) + (x[n + 1] - x[n])) - (Iion[m]) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n - 1] - x[n]) + (x[n + 1] - x[n])) - (Iion[m]) / Cm))) {
-				V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n - 1] - x[n]) + (x[n + 1] - x[n])) - (Iion[m]) / Cm);
-			}
-			else { V[m] = x[n]; }
-		}
-	}
+	// if (width == 1) {
+	// 	if (idx == 0) { //first + stimulus
+	// 		if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n + 1] - x[n])) - (Iion[m] + Istim1) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n + 1] - x[n])) - (Iion[m] + Istim1) / Cm))) {
+	// 			V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n + 1] - x[n])) - (Iion[m] + Istim1) / Cm);
+	// 			//V[m] = (x[n]) + (step)*( rad/(2*rho*Cm*deltx*deltx)*(x[n+1]-2*x[n] + x[n+length-1]) - (Iion[n]+Istim1) /Cm ) ; // loop
+	// 		}
+	// 		else { V[m] = x[n]; }
+	// 	}
+	// 	else if (idx == num_cells - 1){ //last
+	// 		if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(-x[n] + x[n - 1]) - (Iion[m]) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(-x[n] + x[n - 1]) - (Iion[m]) / Cm))) {
+	// 			V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(-x[n] + x[n - 1]) - (Iion[m]) / Cm);
+	// 			//V[m] = (x[n]) + (step)*( rad/(2*rho*Cm*deltx*deltx)*(x[n+1-length] - 2*x[n] + x[n-1]) - (Iion[n]) /Cm ); // loop
+	// 		}
+	// 		else { V[m] = x[n]; }
+	// 	}
+	// 	else if (idx == 1) { //need stimulus
+	// 		if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm))) {
+	// 			V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm);
+	// 		}
+	// 		else { V[m] = x[n]; }
+	// 	}
+	// 	else if (idx == 2) { //need stimulus
+	// 		if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm))) {
+	// 			V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim1) / Cm);
+	// 		}
+	// 		else { V[m] = x[n]; }
+	// 	}
+	// 	//stim2
+	// 	else if (s2_analysis && s2_loc == idx) { //need stimulus
+	// 		if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm))) {
+	// 			V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm);
+	// 		}
+	// 		else { V[m] = x[n]; }
+	// 	}
+	// 	else if (s2_analysis && s2_loc + 1 == idx) { //need stimulus
+	// 		if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm))) {
+	// 			V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm);
+	// 		}
+	// 		else { V[m] = x[n]; }
+	// 	}
+	// 	else if (s2_analysis && s2_loc - 1 == idx) { //need stimulus
+	// 		if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm))) {
+	// 			V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(x[n + 1] - 2 * x[n] + x[n - 1]) - (Iion[m] + Istim2) / Cm);
+	// 		}
+	// 		else { V[m] = x[n]; }
+	// 	}
+	// 	else {
+	// 		if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n - 1] - x[n]) + (x[n + 1] - x[n])) - (Iion[m]) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n - 1] - x[n]) + (x[n + 1] - x[n])) - (Iion[m]) / Cm))) {
+	// 			V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*((x[n - 1] - x[n]) + (x[n + 1] - x[n])) - (Iion[m]) / Cm);
+	// 		}
+	// 		else { V[m] = x[n]; }
+	// 	}
+	// }
 	//Tissue Model
-	else {
-		// set which cells will get a stimulus
-		if (idx == 0 || idx == 1)					{ stim = Istim1; }
-		if (idx == 2 || idx == 0 + length)			{ stim = Istim1; }
-		if (idx == 1 + length || idx == 2 + length)			{ stim = Istim1; }
-		if (idx == 0 + 2 * length || idx == 1 + 2 * length)			{ stim = Istim1; }
-		if (idx == 2 + 2 * length)											{ stim = Istim1; }
+	
+	// set which cells will get a stimulus
+	
+	if(blockIdx.y == 0 && threadIdx.x < 5 && threadIdx.y < 5)
+		stim = Istim1;
+
+	// if (idx == 0 || idx == 1)					{ stim = Istim1; }
+	// if (idx == 2 || idx == 0 + length)			{ stim = Istim1; }
+	// if (idx == 1 + length || idx == 2 + length)			{ stim = Istim1; }
+	// if (idx == 0 + 2 * length || idx == 1 + 2 * length)			{ stim = Istim1; }
+	// if (idx == 2 + 2 * length)											{ stim = Istim1; }
 
 
-		if (threadIdx.x >= 0 && threadIdx.x <= length - 1) { // Top Edge
-			Vnet_U = 0.0;
-		}
-		else {
-			Vnet_U = x[n - length] - x[n];
-		}
-
-
-		if (threadIdx.x >= ((width*length) - length) && threadIdx.x <= ((width*length) - 1)) { // Bottom Edge
-			Vnet_D = 0.0;
-		}
-		else {
-			Vnet_D = x[n + length] - x[n];
-		}
-
-
-		if (threadIdx.x%length == 0) { // Left Edge
-			Vnet_L = 0.0;
-			//Vnet_L =  x[n+length-1] - x[n]; // tissue loop
-		}
-		else {
-			Vnet_L = x[n - 1] - x[n];
-		}
-
-		if (threadIdx.x%length == (length - 1)) { // Right Edge
-			Vnet_R = 0.0;
-			//Vnet_R = x[n+1-length] - x[n]; // tissue loop
-		}
-		else {
-			Vnet_R = x[n + 1] - x[n];
-		}
-
-		if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(Vnet_R + Vnet_L + Vnet_U + Vnet_D) - (Iion[m] + stim) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(Vnet_R + Vnet_L + Vnet_U + Vnet_D) - (Iion[m] + stim) / Cm))) {
-			V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(Vnet_R + Vnet_L + Vnet_U + Vnet_D) - (Iion[m] + stim) / Cm);
-		}
-		else { V[m] = x[n]; }
+	if (blockIdx.y == 0 && threadIdx.y == 0) { // Top Edge
+		Vnet_U = 0.0;
 	}
+	else {
+		Vnet_U = x[n - length] - x[n];
+	}
+
+
+	if (blockIdx.y == gridDim.y - 1 && threadIdx.y == blockDim.y - 1) { // Bottom Edge
+		Vnet_D = 0.0;
+	}
+	else {
+		Vnet_D = x[n + length] - x[n];
+	}
+
+
+	if (threadIdx.x == 0) { // Left Edge
+		Vnet_L = 0.0;
+		//Vnet_L =  x[n+length-1] - x[n]; // tissue loop
+	}
+	else {
+		Vnet_L = x[n - 1] - x[n];
+	}
+
+	if (threadIdx.x == (blockDim.x - 1)) { // Right Edge
+		Vnet_R = 0.0;
+		//Vnet_R = x[n+1-length] - x[n]; // tissue loop
+	}
+	else {
+		Vnet_R = x[n + 1] - x[n];
+	}
+
+	if (!isinf((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(Vnet_R + Vnet_L + Vnet_U + Vnet_D) - (Iion[m] + stim) / Cm)) && !isnan((x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(Vnet_R + Vnet_L + Vnet_U + Vnet_D) - (Iion[m] + stim) / Cm))) {
+		V[m] = (x[n]) + (step)*(rad / (2 * rho*Cm*deltx*deltx)*(Vnet_R + Vnet_L + Vnet_U + Vnet_D) - (Iion[m] + stim) / Cm);
+	}
+	else { V[m] = x[n]; }
+
 	if(local && V[m] >= threshold && passed[m] == 0)
 	{
 		passed[m] = time;
@@ -667,7 +671,7 @@ __global__ void make_randomNums( float* randArray, int num_cells, int num_changi
 	int idx = (simulations + block_number) * num_changing_vars + thread_number;
 
 	float sigma1D = 0.15; //SD of variation of simulation
-	float sigma2D = 0; //SD of variation of cells within cable
+	float sigma2D = 0.15; //SD of variation of cells within cable
 	int i;
 	
 
@@ -715,6 +719,7 @@ int main(int argc, const char* argv[])
 	FILE *s2output = fopen("tt06_GPU_s2_Analysis.txt", "w");
 	FILE* p = fopen("tt06_GPU_passed_threshold.txt", "w");
 	FILE* v = fopen("tt06_GPU_local_velocity.txt","w");
+	FILE* timer = fopen("tt06 Timer","w");
 	int index = 0;
 	float* host_vars;
 	float* dev_vars;
@@ -747,11 +752,11 @@ int main(int argc, const char* argv[])
 	int num_param = 19;
 
 	// Assume only running 1 simulation initially
-	int simulations = 500;
+	int simulations = 2;
 
 	// Time Step Variables
 	float step = 0.002;
-	float tend = 1000;
+	float tend = 50;
 	int iterations = tend / step;
 	float skip_time_value = 0.5; //ms
 	int skip_timept = skip_time_value / step; // skipping time points in voltage array & time array
@@ -766,7 +771,7 @@ int main(int argc, const char* argv[])
 
 	//Stimulus Variables
 	float stimDur = 2.0;
-	float stimAmp = -60.0;
+	float stimAmp = -400;
 	float stimInterval = 1000;
 	int tstim = stimInterval / step;
 
@@ -853,18 +858,22 @@ int main(int argc, const char* argv[])
 	//Set up block and grid dimmensions
 	dim3 dimGrid(simulations,gridDim_y); //Each simulation has 10 blocks
 	dim3 dimBlock(length,blockDim_y); //creates a 10 x 100 block
-	printf("Done initalizing, Begining simulations\n");
+	
 	// Initialize vars array with initial conditions
 	initialConditions <<<dimGrid,dimBlock>>>(dev_vars, num_param, num_cells, dev_passed);
-
+	printf(cudaGetErrorString(cudaGetLastError()));
+	printf("\n");
+	printf("Done initalizing, Begining simulations\n");
+	
 	while (time<iterations) {
 
 		computeState <<<dimGrid,dimBlock>>>(dev_vars, dev_ion_currents, num_cells, step, dev_randNums, simulations, dev_x_temp, num_changing_vars);
+		cudaDeviceSynchronize();
 		updateState <<<dimGrid,dimBlock  >>>(dev_vars, dev_x_temp, num_cells);
 
 		compute_voltage <<<dimGrid,dimBlock >>>(dev_vars, dev_Vtemp, dev_ion_currents, step, dev_randNums, simulations, length, width, num_changing_vars, time, stimDur, stimAmp, tstim, local, dev_passed, threshold);
 		update_voltage <<<dimGrid,dimBlock>>>(dev_vars, dev_Vtemp, num_cells);
-		cudaDeviceSynchronize();
+		
 		//update Voltage and time arrays and write data to file
 
 		if (time%skip_timept == 0) {
@@ -905,6 +914,7 @@ int main(int argc, const char* argv[])
 
 	printf("Elapsed Time = %f s \n", elapsedTime / 1000);
 	printf("\n");
+	fprintf(timer,"Elapsed Time = %f s \n", elapsedTime / 1000);
 	printf("Calculating Simulation outputs...\n");
 	printf("\n");
 
